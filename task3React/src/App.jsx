92f45fa03df6c4e218e4386c66c4725e95ed7500
igorrm23 <igormachadofire14@gmail.com>
2026-02-05 09:30:21 -0300

    primeiro commit

---
+++ task3React/src/App.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+
+function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then((response) => response.json()).then((data) => {
+        setUserData(data.results[0]);
+      }).catch((error) => {
+        console.error("Um erro aconteceu ao buscar dados na api ", error)
+        setUserData(111);
+      })
+
+  }, []);
+
+
+  if (userData === null) {
+    return (
+      <>
+        <p>Carregando.....</p>
+      </>
+    )
+  }
+
+  if (userData === 111) {
+    return (
+      <>
+        <p>Erro 404</p>
+      </>
+    )
+  }
+
+  return (
+    <>
+
+      <div style={{ border: "1px solid #ccc", padding: "16px", width: "250px" }}>
+        <img
+          src={userData.picture.large}
+          alt="User"
+          style={{ borderRadius: "50%" }}
+        />
+
+        <h2>
+          {userData.name.first} {userData.name.last}
+        </h2>
+
+        <p>{userData.email}</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
